00341792314b8cf3dab13328328719be662465e0
Natsulelou <nutshapon_lelou@hotmail.com>
2019-01-08 15:34:01 +0700

    cm

--- src/app/components/component-frames-profile/component-frames-profile.component.ts
+++ src/app/components/component-frames-profile/component-frames-profile.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-export interface Frame {
-  id: string;
-  image: string;
-}
+// export interface Frame {
+//   id: string;
+//   image: string;
+// }
 
 
 @Component({
@@ -32,17 +32,17 @@ export class ComponentFramesProfileComponent implements OnInit {
     console.log(this.frame);
   }
 
-  image: Frame[] = [
-    { id: "1", image: "./assets/img/frame-0.png" },
-    { id: "2", image: "./assets/img/frame-1.png" },
-    { id: "3", image: "./assets/img/frame-2.png" },
-    { id: "4", image: "./assets/img/frame-3.png" },
-    { id: "5", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922692/Rabu%20Rabu/f1.png" },
-    { id: "6", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922690/Rabu%20Rabu/f3.png" },
-    { id: "7", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922692/Rabu%20Rabu/f4.png" },
-    { id: "8", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922692/Rabu%20Rabu/f1.png" },
+  // image: Frame[] = [
+  //   { id: "1", image: "./assets/img/frame-0.png" },
+  //   { id: "2", image: "./assets/img/frame-1.png" },
+  //   { id: "3", image: "./assets/img/frame-2.png" },
+  //   { id: "4", image: "./assets/img/frame-3.png" },
+  //   { id: "5", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922692/Rabu%20Rabu/f1.png" },
+  //   { id: "6", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922690/Rabu%20Rabu/f3.png" },
+  //   { id: "7", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922692/Rabu%20Rabu/f4.png" },
+  //   { id: "8", image: "https://res.cloudinary.com/dyiuidzsc/image/upload/v1546922692/Rabu%20Rabu/f1.png" },
 
-  ]
+  // ]
 
   select(img) {
     // console.log(img)
