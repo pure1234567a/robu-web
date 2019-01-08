99a4544db2e1ba0e3112c22e6b146f40b6b8b3d2
pure1234567a <pure1234567a@gmail.com>
2019-01-08 14:30:50 +0700

    p

--- src/app/components/component-profile/component-profile.component.ts
+++ src/app/components/component-profile/component-profile.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-component-profile',
   templateUrl: './component-profile.component.html',
   styleUrls: ['./component-profile.component.css']
 })
-export class ComponentProfileComponent implements OnInit {
+export class ComponentProfileComponent implements OnInit, AfterViewChecked {
 
   @Input() getFrameImg: any = [];
 
@@ -16,8 +16,12 @@ export class ComponentProfileComponent implements OnInit {
   ngOnInit() {
   }
 
-  clickTest() {
+  ngAfterViewChecked() {
     console.log(this.getFrameImg);
+  }
+
+  clickTest() {
+
 
   }
 
