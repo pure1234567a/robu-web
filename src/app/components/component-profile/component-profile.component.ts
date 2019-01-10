990f88efc3e3c0b3a9b06d9b8dbc4c55fa8af473
pure1234567a <pure1234567a@gmail.com>
2019-01-10 10:55:59 +0700

    get base 64

--- src/app/components/component-profile/component-profile.component.ts
+++ src/app/components/component-profile/component-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, Input, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-component-profile',
@@ -9,16 +9,14 @@ export class ComponentProfileComponent implements OnInit, AfterViewChecked {
 
   @Input() getFrameImg: any = [];
   user: any;
+  userImg: "https://scontent.fbkk12-1.fna.fbcdn.net/v/t1.0-9/33037353_1774105469351187_6162166278820724736_n.jpg?_nc_cat=106&_nc_ht=scontent.fbkk12-1.fna&oh=2a5f4827d44e73fcc805042ec15d83d4&oe=5CC6C4D2"
+  @ViewChild('canvas') canvas: ElementRef
 
   constructor() {
 
   }
 
   ngOnInit() {
-    this.user = JSON.parse(window.localStorage.getItem('@user'))
-    if (this.user) {
-      console.log(this.user);
-    }
 
   }
 
@@ -26,9 +24,41 @@ export class ComponentProfileComponent implements OnInit, AfterViewChecked {
     // console.log(this.getFrameImg);
   }
 
-  clickTest() {
+  clickToMerge() {
+    // console.log(this.getFrameImg);
+    var canvas: any = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
 
+    var img1 = loadImage('https://scontent.fbkk12-1.fna.fbcdn.net/v/t1.0-9/33037353_1774105469351187_6162166278820724736_n.jpg?_nc_cat=106&_nc_ht=scontent.fbkk12-1.fna&oh=2a5f4827d44e73fcc805042ec15d83d4&oe=5CC6C4D2', main);
+    var img2 = loadImage(this.getFrameImg.image.url, main);
 
+    var imagesLoaded = 0;
+    function main() {
+      imagesLoaded = imagesLoaded + 1;
+
+      if (imagesLoaded == 2) {
+        // composite now
+        ctx.drawImage(img1, 0, 0, img1.width, img1.height, 0, 0, canvas.width, canvas.height);
+
+        ctx.globalAlpha = 1;
+        ctx.drawImage(img2, 0, 0, img2.width, img2.height, 0, 0, canvas.width, canvas.height);
+      }
+    }
+
+    function loadImage(src, onload) {
+      var img = new Image();
+
+      img.onload = onload;
+      img.crossOrigin = "Anonymous";
+      img.src = src;
+      return img;
+    }
+  }
+
+  getMerge() {
+    var canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    var dataURL = canvas.toDataURL();
+    console.log(dataURL);
   }
 
 }
