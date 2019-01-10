53b28b0e2eb8af0bde8f96c00472e694cf523864
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-01-10 12:16:04 +0700

    []

--- src/app/components/component-change-profile-facebook/component-change-profile-facebook.component.ts
+++ src/app/components/component-change-profile-facebook/component-change-profile-facebook.component.ts
@@ -32,13 +32,13 @@ export class ComponentChangeProfileFacebookComponent implements OnInit {
     //   // }
 
     // );
-    this.fb.api('/{page-id}/photos', 'post', {
+    this.fb.api('/' + this.user.id + '/photos', 'post', {
       'url': 'https:\/\/www.khaosod.co.th\/wp-content\/uploads\/2016\/09\/14333162_320123348342430_1546313821850115366_n.jpg',
       'published': false
     }).then(res => {
       console.log('res : ', res);
     }).catch(err => {
-      console.log('err : ',err);
+      console.log('err : ', err);
     });
   }
 
