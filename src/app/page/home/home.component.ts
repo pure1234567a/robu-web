5e87a09637ed3cc4ecb6f4fac6d722acb430558d
pure1234567a <pure1234567a@gmail.com>
2019-01-14 16:40:59 +0700

    p

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -11,8 +11,8 @@ export class HomeComponent implements OnInit {
   setframe: Boolean = false;
   frameImg: any;
   user: any;
-  // userImg: any = "https://scontent.fbkk12-1.fna.fbcdn.net/v/t1.0-9/33037353_1774105469351187_6162166278820724736_n.jpg?_nc_cat=106&_nc_ht=scontent.fbkk12-1.fna&oh=2a5f4827d44e73fcc805042ec15d83d4&oe=5CC6C4D2"
-  userImg: any;
+  userImg: any = "https://scontent.fbkk12-1.fna.fbcdn.net/v/t1.0-9/33037353_1774105469351187_6162166278820724736_n.jpg?_nc_cat=106&_nc_ht=scontent.fbkk12-1.fna&oh=2a5f4827d44e73fcc805042ec15d83d4&oe=5CC6C4D2"
+  // userImg: any;
   @ViewChild('canvas') canvas: ElementRef
   dataURL: any;
   imageArray: any;
@@ -53,19 +53,19 @@ export class HomeComponent implements OnInit {
       img.onload = onload;
       img.crossOrigin = "Anonymous";
       img.src = src;
-      console.log('2');
+      console.log('1');
       return img;
     }
 
     function main() {
       imagesLoaded = imagesLoaded + 1;
-      console.log('0');
+      console.log('2');
       if (imagesLoaded == 2) {
         // composite now
         ctx.drawImage(img1, 0, 0, img1.width, img1.height, 0, 0, canvas.width, canvas.height);
         ctx.globalAlpha = 1;
         ctx.drawImage(img2, 0, 0, img2.width, img2.height, 0, 0, canvas.width, canvas.height);
-        console.log('1');
+        console.log('3');
       }
     }
 
@@ -82,7 +82,7 @@ export class HomeComponent implements OnInit {
     this.images = this.dataURL;
     // console.log(this.images)
     this.spinner.hide();
-    console.log('3');
+    console.log('4');
   }
 
 }
