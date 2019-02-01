8fdc85da33a0c14b27cf82a914cfd5d4967575f9
Joseboom <boom.jaidee@hotmail.com>
2019-02-01 16:47:38 +0700

    puu

--- src/app/components/shared/shared.component.ts
+++ src/app/components/shared/shared.component.ts
@@ -21,9 +21,9 @@ export class SharedComponent implements OnInit {
   constructor(
     private fb: FacebookService,
     private spinner: NgxSpinnerService
-    ) {
+  ) {
     const initParams: InitParams = {
-      appId: '2134431583284588',
+      appId: '391466811398230',
       version: 'v3.2'
     };
 
@@ -44,26 +44,26 @@ export class SharedComponent implements OnInit {
   //   element.click();
   // }
   shareWithOpenGraphActions() {
-    
-    this.pushUpload(this.imageInput).then(res => {
-      const params: UIParams = {
-        method: 'share',
-        action_type: 'og.likes',
-        action_properties: JSON.stringify({
-          object: {
-            // 'og:url': 'https://angular-for-seo.firebaseapp.com',
-            'og:title': 'RabuRabuLoveLove',
-            'og:description': 'ข้อความยาว ๆ',
-            'og:image': res
-          }
-        })
-      };
-      this.fb.ui(params)
-        .then((res: UIResponse) => console.log(res))
-        .catch((e: any) => console.error(e));
-    }).catch(err => {
-      console.log(err)
-    });
+
+    // this.pushUpload(this.imageInput).then(res => {
+    const params: UIParams = {
+      method: 'share',
+      action_type: 'og.likes',
+      action_properties: JSON.stringify({
+        object: {
+          // 'og:url': 'https://angular-for-seo.firebaseapp.com',
+          'og:title': 'RabuRabuLoveLove',
+          'og:description': 'ข้อความยาว ๆ',
+          'og:image': 'https://cache.gmo2.sistacafe.com/images/uploads/summary/image/41751/3e9fea8de38ad5631fd691a1e9c54c26.jpg'
+        }
+      })
+    };
+    this.fb.ui(params)
+      .then((res: UIResponse) => console.log(res))
+      .catch((e: any) => console.error(e));
+    // }).catch(err => {
+    //   console.log(err)
+    // });
 
     // const image = await this.pushUpload(this.imageInput);
     // console.log(image);
@@ -84,19 +84,19 @@ export class SharedComponent implements OnInit {
     //   .catch((e: any) => console.error(e));
 
   }
-  pushUpload(base64) {
-    this.spinner.show()
-    return new Promise((resove, reject) => {
-      const storageRef = firebase.storage().ref();
-      const fileRandom = Math.floor((Date.now() / 1000) + new Date().getUTCMilliseconds());
-      const uploadTask: any = storageRef.child(`images/uploads/${fileRandom}.jpg`);
-      uploadTask.putString(base64, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
-        uploadTask.getDownloadURL().then(url => {
-          resove(url);
-        });
-      });
-      this.spinner.hide()
-    });
-  }
+  // pushUpload(base64) {
+  //   this.spinner.show()
+  //   return new Promise((resove, reject) => {
+  //     const storageRef = firebase.storage().ref();
+  //     const fileRandom = Math.floor((Date.now() / 1000) + new Date().getUTCMilliseconds());
+  //     const uploadTask: any = storageRef.child(`images/uploads/${fileRandom}.jpg`);
+  //     uploadTask.putString(base64, firebase.storage.StringFormat.DATA_URL).then((snapshot) => {
+  //       uploadTask.getDownloadURL().then(url => {
+  //         resove(url);
+  //       });
+  //     });
+  //     this.spinner.hide()
+  //   });
+  // }
 
 }
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -19,7 +19,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private route: Router,
-  ) { 
+  ) {
   }
 
   signInWithFB(): void {
@@ -34,10 +34,38 @@ export class LoginComponent implements OnInit {
     this.signInWithFB();
     this.authService.authState.subscribe((user) => {
       this.user = user;
-      this.user.photoUrl = "https://graph.facebook.com/"+this.user.id+"/picture?width=2000&height=2000"
-      window.localStorage.setItem('@user',JSON.stringify(this.user));
+      this.user.photoUrl = "https://graph.facebook.com/" + this.user.id + "/picture?width=2000&height=2000"
+      window.localStorage.setItem('@user', JSON.stringify(this.user));
       this.loggedIn = (user != null);
       this.route.navigate(['home'])
     });
+    this.saveUser();
+  }
+  saveUser() {
+    try {
+      let dataUser = JSON.parse(window.localStorage.getItem('@user'));
+      console.log(dataUser);
+      let data = {
+        username: dataUser.email ? dataUser.email : dataUser.firstName + dataUser.lastName,
+        password: 'P@ssw0rd'
+      }
+      let user: any = this.seviceApi.saveUser(data);
+      console.log('login'+user)
+      console.log('login')
+      if (!user) {
+        console.log('regis');
+        let dataRegis = {
+          username: dataUser.email ? dataUser.email : dataUser.firstName + dataUser.lastName,
+          password: 'P@ssw0rd',
+          firstname: dataUser.firstName,
+          lastname: dataUser.lastName,
+          email: dataUser.email ? dataUser.email : dataUser.firstName + dataUser.lastName + '@hotmail.com'
+        }
+        let res: any = this.seviceApi.sigup(dataRegis)
+        console.log('resiter'+res)
+      }
+    } catch (error) {
+      console.log('error '+error)
+    }
   }
 }
